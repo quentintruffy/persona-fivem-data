81375e4c770e5c9d0c1e84f8b3ac1093ed12ec21
quent <quentin.truffy@7opteam.com>
2025-03-09 18:51:42 +0100

    .

---
+++ resources/[local]/persona/fxmanifest.lua
@@ -0,0 +1,9 @@
+fx_version 'cerulean'
+game 'gta5'
+
+author 'Quentin TRUFFY <pro.quentint@gmail.com>'
+description 'Description de votre plugin'
+version '1.0.0'
+
+client_script 'client.lua'
+server_script 'server.lua'
